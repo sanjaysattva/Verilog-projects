41708f1e9283baaccf850200aa08d2505e9b0d6a
sanjaysattva <85502194+sanjaysattva@users.noreply.github.com>
2022-07-06 10:57:58 +0530

    Update alu.v

--- ALU/alu.v
+++ ALU/alu.v
@@ -34,7 +34,7 @@ module ripplecarry(
     fa0(a[0], b[0],cin, s[0],cout0),
     fa1(a[1], b[1], cout0, s[1],cout1),
     fa2(a[2], b[2], cout1, s[2],cout2),
-    fa3(a[3], b[3], cout2, s[3],cout),
+    fa3(a[3], b[3], cout2, s[3],cout);
 endmodule
 
 
@@ -48,7 +48,7 @@ module adder8(
   wire cout3;
   ripplecarry
   ra0(cin,a[3:0], b[3:0], s[3:0],cout3),
-  ra1(cout3,a[7:4], b[7:4], s[7:4],cout),
+  ra1(cout3,a[7:4], b[7:4], s[7:4],cout);
 endmodule
 
 module subtractor8(
@@ -154,3 +154,5 @@ endmodule
 
 
 
+  
+  
