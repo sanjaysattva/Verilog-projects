3e06612e4f15f3fc75e3ce7e58f73b50d39ee9bf
sanjaysattva <85502194+sanjaysattva@users.noreply.github.com>
2022-07-27 15:47:39 +0530

    Update ripple.v

--- ripple_adder/ripple.v
+++ ripple_adder/ripple.v
@@ -29,3 +29,11 @@ module ripplecarry(
   output [3:0] s,
   output cout
 );
+  
+  wire cout0,cout1,cout2;
+  fulladder
+    fa0(a[0], b[0],cin, s[0],cout0),
+    fa1(a[1], b[1], cout0, s[1],cout1),
+    fa2(a[2], b[2], cout1, s[2],cout2),
+    fa3(a[3], b[3], cout2, s[3],cout);
+endmodule
